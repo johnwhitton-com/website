faab6eb64476e10f2b0f98c642f21fe6344bbdf6
johnwhitton <john@johnwhitton.com>
2025-07-21 11:37:47 -0700

    Page cleanup WIP

---
+++ docs/dist/assets/celestia-DuhakgXY.js
@@ -0,0 +1,95 @@
+import{u as t,j as e}from"./index-DGpkqePg.js";const s={title:"Celestia",description:"undefined"};function r(n){const i={a:"a",div:"div",h1:"h1",h2:"h2",h3:"h3",header:"header",li:"li",ol:"ol",p:"p",section:"section",sup:"sup",ul:"ul",...t(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(i.header,{children:e.jsxs(i.h1,{id:"celestia",children:["Celestia",e.jsx(i.a,{"aria-hidden":"true",tabIndex:"-1",href:"#celestia",children:e.jsx(i.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(i.p,{children:"date: 2023-06-19"}),`
+`]}),`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(i.p,{children:"last updated: 2023-06-19"}),`
+`]}),`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(i.p,{children:e.jsx(i.a,{href:"#overview",children:"Overview"})}),`
+`]}),`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(i.p,{children:e.jsx(i.a,{href:"#functionality",children:"Functionality"})}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"#modular",children:"Modular"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(i.p,{children:e.jsx(i.a,{href:"#code-review",children:"Code Review"})}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"#celestia-node",children:"Celestia Node"})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"#rollkit",children:"RollKit"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(i.p,{children:e.jsx(i.a,{href:"#deployment",children:"Deployment"})}),`
+`]}),`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(i.p,{children:e.jsx(i.a,{href:"#cryptographic-primitives",children:"Cryptographic Primitives"})}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"#namespaced-merkle-tree-wrapper",children:"Namespaced Merkle Tree Wrapper"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(i.p,{children:e.jsx(i.a,{href:"#appendices",children:"Appendices"})}),`
+`]}),`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(i.p,{children:e.jsx(i.a,{href:"#references",children:"References"})}),`
+`]}),`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(i.p,{children:e.jsx(i.a,{href:"#footnotes",children:"Footnotes"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(i.h2,{id:"overview",children:["Overview",e.jsx(i.a,{"aria-hidden":"true",tabIndex:"-1",href:"#overview",children:e.jsx(i.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(i.p,{children:["Celestia",e.jsx(i.sup,{children:e.jsx(i.a,{href:"#user-content-fn-ov-1",id:"user-content-fnref-ov-1","data-footnote-ref":!0,"aria-describedby":"footnote-label",children:"1"})})," is a modular consensus and data network, built to enable anyone to easily deploy their own blockchain with minimal overhead."]}),`
+`,e.jsxs(i.h2,{id:"functionality",children:["Functionality",e.jsx(i.a,{"aria-hidden":"true",tabIndex:"-1",href:"#functionality",children:e.jsx(i.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(i.h3,{id:"modular",children:["Modular",e.jsx(i.a,{"aria-hidden":"true",tabIndex:"-1",href:"#modular",children:e.jsx(i.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(i.h2,{id:"code-review",children:["Code Review",e.jsx(i.a,{"aria-hidden":"true",tabIndex:"-1",href:"#code-review",children:e.jsx(i.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(i.h3,{id:"celestia-node",children:["Celestia Node",e.jsx(i.a,{"aria-hidden":"true",tabIndex:"-1",href:"#celestia-node",children:e.jsx(i.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(i.h3,{id:"rollkit",children:["RollKit",e.jsx(i.a,{"aria-hidden":"true",tabIndex:"-1",href:"#rollkit",children:e.jsx(i.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(i.h2,{id:"deployment",children:["Deployment",e.jsx(i.a,{"aria-hidden":"true",tabIndex:"-1",href:"#deployment",children:e.jsx(i.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(i.h2,{id:"cryptographic-primitives",children:["Cryptographic Primitives",e.jsx(i.a,{"aria-hidden":"true",tabIndex:"-1",href:"#cryptographic-primitives",children:e.jsx(i.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[e.jsx(i.a,{href:"https://arxiv.org/pdf/1809.09044.pdf",children:"whitepaper"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:"2.2 Merkle Trees and Sparse Merkle Trees"}),`
+`,e.jsx(i.li,{children:"2.3 Erasure Codes and Reed-Solomon Codes"}),`
+`,e.jsxs(i.li,{children:["5 Data Availability Proofs",`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:"5.1 Strawman 1D Reed-Solomon Availability Scheme"}),`
+`,e.jsx(i.li,{children:"5.2 2D Reed-Solomon Encoded Merkle Tree Construction"}),`
+`,e.jsx(i.li,{children:"5.3 Random Sampling and Network Block Recovery"}),`
+`,e.jsx(i.li,{children:"5.4 Selective Share Disclosure"}),`
+`,e.jsx(i.li,{children:"5.5 Fraud Proofs of Incorrectly Generated Extended Data"}),`
+`]}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/celestiaorg/celestia-app/tree/main/docs/architecture",children:"Celestia App Architecture"})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/celestiaorg/celestia-app/tree/main/pkg",children:"Implementation celestia-app pkg"})}),`
+`]}),`
+`,e.jsxs(i.h3,{id:"namespaced-merkle-tree-wrapper",children:["Namespaced Merkle Tree Wrapper",e.jsx(i.a,{"aria-hidden":"true",tabIndex:"-1",href:"#namespaced-merkle-tree-wrapper",children:e.jsx(i.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/celestiaorg/celestia-app/tree/main/pkg/wrapper",children:"code"})}),`
+`]}),`
+`,e.jsxs(i.h2,{id:"appendices",children:["Appendices",e.jsx(i.a,{"aria-hidden":"true",tabIndex:"-1",href:"#appendices",children:e.jsx(i.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(i.h2,{id:"references",children:["References",e.jsx(i.a,{"aria-hidden":"true",tabIndex:"-1",href:"#references",children:e.jsx(i.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://celestia.org/learn/",children:"Learn Modular"})}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.a,{href:"https://arxiv.org/pdf/1809.09044.pdf",children:"Fraud and Data Availability Proofs"}),": Maximising Light Client Security and Scaling Blockchains with Dishonest Majorities"]}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://celestiaorg.github.io/celestia-app/index.html",children:"Celestia App Specifications"})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/celestiaorg/celestia-app/tree/main/docs/architecture",children:"Celestia App Architecture"})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://rollkit.dev/docs/intro/",children:"Rollkit Docs"})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/rollkit/rollkit/tree/main/docs/lazy-adr",children:"Rollkit Architecture Design Records"})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/rollkit/rollkit/blob/main/docs/specification/rollkit-dependency-graph.md",children:"Rollkit Dependency Graph"})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/celestiaorg/quantum-gravity-bridge/tree/master",children:"Quantum Gravity Bridge"})}),`
+`]}),`
+`,e.jsxs(i.h2,{id:"footnotes",children:["Footnotes",e.jsx(i.a,{"aria-hidden":"true",tabIndex:"-1",href:"#footnotes",children:e.jsx(i.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(i.section,{"data-footnotes":!0,className:"footnotes",children:[e.jsxs(i.h2,{className:"sr-only",id:"footnote-label",children:["Footnotes",e.jsx(i.a,{"aria-hidden":"true",tabIndex:"-1",href:"#footnote-label",children:e.jsx(i.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(i.ol,{children:[`
+`,e.jsxs(i.li,{id:"user-content-fn-ov-1",children:[`
+`,e.jsxs(i.p,{children:[e.jsx(i.a,{href:"https://celestia.org/",children:"Celestia"}),": the first modular blockchan network. ",e.jsx(i.a,{href:"#user-content-fnref-ov-1","data-footnote-backref":"","aria-label":"Back to reference 1",className:"data-footnote-backref",children:"↩"})]}),`
+`]}),`
+`]}),`
+`]})]})}function a(n={}){const{wrapper:i}={...t(),...n.components};return i?e.jsx(i,{...n,children:e.jsx(r,{...n})}):r(n)}export{a as default,s as frontmatter};
---
+++ docs/dist/assets/snowbridge-CFuftLfc.js
@@ -0,0 +1,178 @@
+import{u as s,j as e}from"./index-DGpkqePg.js";const l={title:"Snowbridge",description:"undefined"};function i(r){const n={a:"a",blockquote:"blockquote",code:"code",div:"div",h1:"h1",h2:"h2",h3:"h3",header:"header",li:"li",p:"p",strong:"strong",ul:"ul",...s(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.header,{children:e.jsxs(n.h1,{id:"snowbridge",children:["Snowbridge",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#snowbridge",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"date: 2023-02-24"}),`
+`,e.jsx(n.li,{children:"last-updated: 2023-02-24"}),`
+`]}),`
+`,e.jsxs(n.h2,{id:"overview",children:["Overview",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#overview",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.p,{children:[`Snowbridge is a general purpose, trustless and decentralized bridge between Polkadot and Ethereum. The goal is to launch as common-good bridge on the proposed BridgeHub parachain.
+`,e.jsx(n.strong,{children:"Quick Links"})]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Bridge Type : Proof Based Validity Proofs"}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://docs.snowbridge.network/architecture/overview",children:"Design"}),": bridge has a layered architecture, inspired by networking protocols such as TCP/IP. At the lowest level we have channels, which send messages across the bridge. At the highest level, we have apps, which can invoke methods on apps living on foreign chains."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://docs.snowbridge.network/",children:"Docs"}),": Snowbridge documentation"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge",children:"Implementation snowfork snowbridge"}),": A trustless bridge between Polkadot and Ethereum."]}),`
+`,e.jsx(n.li,{children:"FronteEnd: Still Under Development"}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://polkadot.subscan.io/parachain",children:"Explorer"}),": Can be used to review if a common good parachain is deployed. Bridge messages will be displayed as Cross Chain Messages (XCM) on this ",e.jsx(n.a,{href:"https://polkadot.subscan.io/xcm_dashboard",children:"xcm dashboard"}),"."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://gateway.pinata.cloud/ipfs/QmfYGxQvyjVrgm9ajfzCysbuvLXdsRxP5R5HFjWcrj2yYY",children:"Roadmap"}),": The ",e.jsx(n.a,{href:"https://polkadot.polkassembly.io/post/1341",children:"Snowbridge Funding Proposal"})," includes a link to the ",e.jsx(n.a,{href:"https://gateway.pinata.cloud/ipfs/QmfYGxQvyjVrgm9ajfzCysbuvLXdsRxP5R5HFjWcrj2yYY",children:"roadmap"})," which details the milestones and rollout."]}),`
+`]}),`
+`,e.jsxs(n.h2,{id:"design-notes",children:["Design Notes",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#design-notes",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsx(n.strong,{children:e.jsx(n.a,{href:"https://docs.snowbridge.network/architecture/verification/polkadot",children:"light-client verification polkadot"})}),`
+`,e.jsx(n.p,{children:`We use Polkadot’s BEEFY gadget to implement an efficient light client that only needs to verify a very small subset of relay chain validator signatures. BEEFY is live on Rococo, and is awaiting deployment on Kusama and Polkadot.
+Fundamentally, the BEEFY light client allows the bridge to prove that a specified parachain header was finalized by the relay chain.`}),`
+`,e.jsx(n.strong,{children:e.jsx(n.a,{href:"https://github.com/paritytech/parity-bridges-common/blob/master/modules/beefy/src/lib.rs",children:"BEEFY light client"})}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:"BEEFY bridge pallet."}),`
+`,e.jsxs(n.p,{children:["This pallet is an on-chain BEEFY light client for Substrate-based chains that are using the following pallets bundle: ",e.jsx(n.code,{children:"pallet-mmr"}),", ",e.jsx(n.code,{children:"pallet-beefy"})," and ",e.jsx(n.code,{children:"pallet-beefy-mmr"}),"."]}),`
+`,e.jsx(n.p,{children:"The pallet is able to verify MMR leaf proofs and BEEFY commitments, so it has access to the following data of the bridged chain:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"header hashes"}),`
+`,e.jsx(n.li,{children:"changes of BEEFY authorities"}),`
+`,e.jsx(n.li,{children:"extra data of MMR leafs"}),`
+`]}),`
+`,e.jsx(n.p,{children:"Given the header hash, other pallets are able to verify header-based proofs (e.g. storage proofs, transaction inclusion proofs, etc.)."}),`
+`]}),`
+`,e.jsx(n.strong,{children:e.jsx(n.a,{href:"https://docs.snowbridge.network/architecture/verification/ethereum",children:"light-client verification ethereum"})}),`
+`,e.jsx(n.p,{children:"We have implemented a Proof-of-Stake (PoS) light client for the Beacon chain. This client deprecates the older PoW light client we developed in 2020."}),`
+`,e.jsx(n.p,{children:"The beacon client tracks the beacon chain, the new Ethereum chain that will replace Ethereum's Proof-of-Work consensus method around mid-September, called the Merge. The work we have done consists of the following parts:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Beacon Client pallet",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Initial chain snapshot (forms part of the Genesis Config)"}),`
+`,e.jsx(n.li,{children:"Sync committee updates"}),`
+`,e.jsx(n.li,{children:"Finalized beacon header updates"}),`
+`,e.jsx(n.li,{children:"Execution header updates"}),`
+`,e.jsx(n.li,{children:"Message verification"}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Beacon Relayer",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Sends data from a beacon node to the beacon client"}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.h2,{id:"code-review",children:["Code Review",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#code-review",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.h3,{id:"signing-mechanisms",children:["Signing Mechanisms",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#signing-mechanisms",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://www.secg.org/sec2-v2.pdf",children:"secp256k1"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/tree/main/relayer/crypto/secp256k1",children:"snowbridge secp256k1"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://wiki.polkadot.network/docs/learn-cryptography#what-is-sr25519-and-where-did-it-come-from",children:"sr25519"}),": schnorr over ristretto25519",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/tree/main/relayer/crypto/secp256k1",children:"snowbridge sr25519"})}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.h3,{id:"cryptographic-primitives",children:["Cryptographic Primitives",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#cryptographic-primitives",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://keccak.team/keccak.html",children:"keccak256"})," hash function",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/crypto/keccak/keccak.go",children:"snowbridge keccak"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://en.wikipedia.org/wiki/Merkle_tree",children:"merkle trees"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/crypto/merkle/merkle.go",children:"snowbridge merkle.go"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/parachain/pallets/ethereum-beacon-client/src/merkleization.rs",children:"snowbridge merkleization.rs"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://github.com/ethereum/consensus-specs/blob/dev/ssz/merkle-proofs.md",children:"merkle_proof"})," ",e.jsx(n.a,{href:"https://soliditydeveloper.com/merkle-tree",children:"explainer"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/crypto/merkle/simplified_mmr_proof.go",children:"snowbridge simplified_mmr_proof.go"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/tree/main/parachain/pallets/basic-channel/merkle-proof",children:"snowbridge merkle-proof rust"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/core/packages/contracts/contracts/utils/MerkleProof.sol",children:"snowbridge MerkleProof.sol"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://docs.grin.mw/wiki/chain-state/merkle-mountain-range/",children:"Merkle Mountain Range"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/core/packages/contracts/contracts/utils/MMRProof.sol",children:"snowbridge MMRProof.sol"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/merkle-mountain-range",children:"snowfork merkle-mountain-range"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://ethereum.org/en/developers/docs/data-structures-and-encoding/ssz/",children:"Simple Serialize"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/parachain/pallets/ethereum-beacon-client/src/ssz.rs",children:"snowbridge ssz.rs"})}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.h3,{id:"proving-mechanisms",children:["Proving Mechanisms",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#proving-mechanisms",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:e.jsx(n.a,{href:"https://wiki.polkadot.network/docs/learn-consensus#bridging-beefy",children:"Beefy"})}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/core/packages/contracts/contracts/BeefyClient.sol",children:"snowbridge BeefyClient.sol"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/tree/main/parachain/pallets/ethereum-beacon-client",children:"snowbridge ethereum-beacon-client rust"})}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/tree/main/parachain/pallets/ethereum-light-client",children:"snowbridge ethereum-light-client rust"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.h3,{id:"relayer-mechanisms",children:["Relayer Mechanisms",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#relayer-mechanisms",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/tree/main/relayer/relays",children:"relays folder"})," has multiple relayers controlled by ",e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/execution/main.go",children:"execution"}),". All written in go."]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["Relaying from Ethereum to Polkadot",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/tree/main/relayer/relays/beacon",children:"beacon"}),": Responsible for retrieving state from the beacon chain including",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/beacon/header/syncer/syncer.go",children:"synching"}),":",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/beacon/header/syncer/syncer.go#L47",children:"Header"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/beacon/header/syncer/syncer.go#L55",children:"CurrentSyncCommittee"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/beacon/header/syncer/syncer.go#LL60C12-L60C12",children:"SyncAggregate"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["functions include",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/beacon/header/syncer/syncer.go#L65",children:"GetSyncPeriodsToFetch"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/beacon/header/syncer/syncer.go#L83",children:"GetInitialSync"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/beacon/header/syncer/syncer.go#L118",children:"GetSyncCommitteePeriodUpdate"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/beacon/header/syncer/syncer.go#L187",children:"GetBlockRoots"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/beacon/header/syncer/syncer.go#L252",children:"GetFinalizedUpdate"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/beacon/header/syncer/syncer.go#L305",children:"HasFinalizedHeaderChanged"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/beacon/header/syncer/syncer.go#L326",children:"GetLatestFinalizedHeader"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/beacon/header/syncer/syncer.go#L379",children:"getNextBlockRootBySlot"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/beacon/header/syncer/syncer.go#L425",children:"GetNextHeaderUpdateBySlotWithAncestryProof"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/beacon/header/syncer/syncer.go#L434",children:"GetNextHeaderUpdateBySlot"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/beacon/header/syncer/syncer.go#L457",children:"GetHeaderUpdateWithAncestryProof"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/beacon/header/syncer/syncer.go#L495",children:"getBlockHeaderAncestryProof"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/beacon/header/syncer/syncer.go#L520",children:"GetSyncAggregate"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/relayer/relays/beacon/header/syncer/syncer.go#L534",children:"GetSyncAggregateForSlot"})}),`
+`]}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/tree/main/relayer/relays/ethereum",children:"ethereum"}),": Responsible for retreiving block headers from the execution chain."]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:["Relaying from Polkadot to Etherem",`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/tree/main/relayer/relays/parachain",children:"parachain"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/tree/main/relayer/relays/beefy",children:"beefy"})}),`
+`]}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.h3,{id:"light-client-functionality",children:["Light Client Functionality",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#light-client-functionality",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:e.jsx(n.a,{href:"https://wiki.polkadot.network/docs/learn-consensus#bridging-beefy",children:"Beefy"})}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/core/packages/contracts/contracts/BeefyClient.sol",children:"snowbridge BeefyClient.sol"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/tree/main/parachain/pallets/ethereum-beacon-client",children:"snowbridge ethereum-beacon-client rust"})}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/tree/main/parachain/pallets/ethereum-light-client",children:"snowbridge ethereum-light-client rust"})}),`
+`]}),`
+`]}),`
+`,e.jsxs(n.h3,{id:"token-lockers",children:["Token Lockers",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#token-lockers",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/Snowfork/snowbridge/blob/main/core/packages/contracts/contracts/ERC20Vault.sol",children:"snowbridge ERC20Vault.sol"})}),`
+`]}),`
+`,e.jsxs(n.h2,{id:"references",children:["References",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#references",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsxs(n.h2,{id:"appendices",children:["Appendices",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#appendices",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})]})}function t(r={}){const{wrapper:n}={...s(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(i,{...r})}):i(r)}export{t as default,l as frontmatter};
