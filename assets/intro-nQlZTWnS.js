0942e97893b22a3f210d375040ef1191917a7187
johnwhitton <john@johnwhitton.com>
2025-07-23 10:06:09 -0700

    Updates

---
+++ assets/intro-nQlZTWnS.js
@@ -0,0 +1,45 @@
+import{u as t,j as i}from"./index-mBGIvZA_.js";const a={title:"Solving and Arbitrage Research",description:"undefined"};function r(e){const n={a:"a",div:"div",h1:"h1",h2:"h2",header:"header",li:"li",p:"p",strong:"strong",ul:"ul",...t(),...e.components};return i.jsxs(i.Fragment,{children:[i.jsx(n.header,{children:i.jsxs(n.h1,{id:"solving-and-arbitrage-research",children:["Solving and Arbitrage Research",i.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#solving-and-arbitrage-research",children:i.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
+`,i.jsxs(n.ul,{children:[`
+`,i.jsx(n.li,{children:"date: 2025-06-22"}),`
+`,i.jsx(n.li,{children:"last updated: 2024-06-22"}),`
+`]}),`
+`,i.jsxs(n.h2,{id:"overview",children:["Overview",i.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#overview",children:i.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,i.jsxs(n.p,{children:["Here you'll find my work in 2025 on Solving, Arbitrage and Indexing using ",i.jsx(n.a,{href:"https://docs.propellerheads.xyz/tycho/overview",children:"Tycho"}),", Intents using ",i.jsx(n.a,{href:"https://www.erc7683.org/spec",children:"ERC-7683"}),", ",i.jsx(n.a,{href:"https://eips.ethereum.org/EIPS/eip-712",children:"EIP-712"}),", ",i.jsx(n.a,{href:"https://github.com/uniswap/compactx",children:"Compactx"})," and ",i.jsx(n.a,{href:"https://docs.uniswap.org/contracts/v4/overview",children:"Uniswap V4 Hooks"}),". As of July 2025 I use ",i.jsx(n.a,{href:"https://github.com/jincubator",children:"jincubator"})," for research and development of prototypes."]}),`
+`,i.jsxs(n.h2,{id:"abstract",children:["Abstract",i.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#abstract",children:i.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,i.jsx(n.p,{children:"Liquidity Fragmentation and Capital Efficiency are areas that can be optimized in Blockchain protocols, with the emergence of Multiple L2 Chains and a shift towards intent-based architectures. There is a greater need than ever for a settlement layer to balance provided liquidity."}),`
+`,i.jsx(n.p,{children:"Project Description: Jincubator (Working Name) provides infrastructure and services for multi-chain, intent-based protocols, leveraging hooks built on Uniswap V4."}),`
+`,i.jsx(n.p,{children:"It aims to provide the following"}),`
+`,i.jsx(n.strong,{children:"Features of the Solution"}),`
+`,i.jsxs(n.ul,{children:[`
+`,i.jsxs(n.li,{children:["Pool Functionality",`
+`,i.jsxs(n.ul,{children:[`
+`,i.jsx(n.li,{children:"JIT Liquidity Provisioning,"}),`
+`,i.jsx(n.li,{children:"Dynamic Fees to reduce arbitrage,"}),`
+`,i.jsx(n.li,{children:"Gas Manager allows users to pay for gas using their swap token instead of the native token,"}),`
+`,i.jsx(n.li,{children:"Incentivization Tools for Pools to incentivize Liquidity Providers,"}),`
+`]}),`
+`]}),`
+`,i.jsxs(n.li,{children:["Order Flow",`
+`,i.jsxs(n.ul,{children:[`
+`,i.jsx(n.li,{children:"Intent Solving infrastructure providing traders with the best value for their swap,"}),`
+`,i.jsx(n.li,{children:"SlowTrack and Fastrak - Settlement Options,"}),`
+`]}),`
+`]}),`
+`,i.jsxs(n.li,{children:["Capital Efficiency",`
+`,i.jsxs(n.ul,{children:[`
+`,i.jsx(n.li,{children:"Settlement Layer with Dynamic Rebalancing of Capital Across Chains,"}),`
+`,i.jsx(n.li,{children:"JIT provisioning of liquidity to pools as needed which integrates with Yield Earning Protocols.,"}),`
+`,i.jsx(n.li,{children:"Arbitraging Liquidity across Pools, Protocols, and Chains"}),`
+`]}),`
+`]}),`
+`]}),`
+`,i.jsx(n.strong,{children:"Solution we are implementing"}),`
+`,i.jsxs(n.ul,{children:[`
+`,i.jsx(n.li,{children:"Intent Based - Cross Chain Solving"}),`
+`,i.jsx(n.li,{children:"Dynamic Cross-Chain Capital Allocation for Liquidity Providers"}),`
+`,i.jsx(n.li,{children:"JIT Liquidity Provisioning for Pools"}),`
+`,i.jsx(n.li,{children:"Yield Earning Capabilities for unitilized liquidity"}),`
+`,i.jsx(n.li,{children:"Centralized Liquidity Pool which is dynamically allocated"}),`
+`,i.jsx(n.li,{children:"Settlement Layer used by Solvers and Liquidity Providers used to rebalance funds across chains."}),`
+`,i.jsx(n.li,{children:"Advanced Indexing and Simulation capabilities to enable a global view of liquidity, swap prices and arbitrage opportunities"}),`
+`]})]})}function o(e={}){const{wrapper:n}={...t(),...e.components};return n?i.jsx(n,{...e,children:i.jsx(r,{...e})}):r(e)}export{o as default,a as frontmatter};
