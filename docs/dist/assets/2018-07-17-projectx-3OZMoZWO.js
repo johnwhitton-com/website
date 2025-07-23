3a7c370c7faa9f7baa1133e7a296b97a3385bae2
johnwhitton <john@johnwhitton.com>
2025-07-22 19:04:54 -0700

    Deployment troubleshooting

--- docs/dist/assets/2018-07-17-projectx-3OZMoZWO.js
+++
@@ -1,7 +0,0 @@
-import{u as r,j as e}from"./index-PsDUS4W9.js";const c={title:"Project-X Technical White Paper",description:"undefined"};function i(n){const t={a:"a",div:"div",h1:"h1",header:"header",li:"li",p:"p",ul:"ul",...r(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.header,{children:e.jsxs(t.h1,{id:"project-x-technical-white-paper",children:["Project-X Technical White Paper",e.jsx(t.a,{"aria-hidden":"true",tabIndex:"-1",href:"#project-x-technical-white-paper",children:e.jsx(t.div,{"data-autolink-icon":!0})})]})}),`
-`,e.jsx(t.p,{children:"Blockchain ledger platform with a focus on interoperability, speed and privacy"}),`
-`,e.jsxs(t.ul,{children:[`
-`,e.jsx(t.li,{children:"date: 2018-07-17"}),`
-`,e.jsx(t.li,{children:"author: John Whitton"}),`
-`]}),`
-`,e.jsx("object",{data:"/posts/2018-07-17-projectx/projectX.pdf",width:"1000",height:"1000",type:"application/pdf"})]})}function o(n={}){const{wrapper:t}={...r(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(i,{...n})}):i(n)}export{o as default,c as frontmatter};
