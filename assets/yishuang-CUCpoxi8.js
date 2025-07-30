56f5af9b962645fd503d78ecd228eabd1c49db35
johnwhitton <john@johnwhitton.com>
2025-07-30 09:56:15 -0700

    Updates

---
+++ assets/yishuang-CUCpoxi8.js
@@ -0,0 +1,7 @@
+import{u as o,j as e}from"./index-BP_nZPkk.js";const a=void 0;function i(t){const n={a:"a",div:"div",h3:"h3",li:"li",ol:"ol",p:"p",...o(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsxs(n.h3,{id:"yishuang",children:[e.jsx(n.a,{href:"https://www.linkedin.com/in/chenyishuang/",children:"Yishuang"}),e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#yishuang",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+`,e.jsx(n.p,{children:"John is a really good team player, among his good qualities, I admire three most."}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsx(n.li,{children:"Helpful and warm-hearted whenever you ask for help, John will give you the best help."}),`
+`,e.jsx(n.li,{children:"Avid learner, John likes to learn new things and will actively take new tasks. I see John actively in ops recently."}),`
+`,e.jsx(n.li,{children:"Responsible, longer view, John focused a lot on knowledge transfer. He wants to make sure the knowledge can be shared with the whole team and future new employees, so he contributed a lot on internal wiki gitbooks, which I think it’s a good point."}),`
+`]})]})}function l(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(i,{...t})}):i(t)}export{l as default,a as frontmatter};
