ea9adc1b0ed529cb4e9f65e801228d989d6cf109
johnwhitton <john@johnwhitton.com>
2025-09-28 15:53:14 -0700

    Updates

--- assets/yishuang-BFz-1vjS.js
+++ assets/yishuang-BFz-1vjS.js
@@ -1,4 +1,4 @@
-import{u as o,j as e}from"./index-CSDomXHI.js";const a=void 0;function i(t){const n={a:"a",div:"div",h3:"h3",li:"li",ol:"ol",p:"p",...o(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsxs(n.h3,{id:"yishuang",children:[e.jsx(n.a,{href:"https://www.linkedin.com/in/chenyishuang/",children:"Yishuang"}),e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#yishuang",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
+import{u as o,j as e}from"./index-CvHNygML.js";const a=void 0;function i(t){const n={a:"a",div:"div",h3:"h3",li:"li",ol:"ol",p:"p",...o(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsxs(n.h3,{id:"yishuang",children:[e.jsx(n.a,{href:"https://www.linkedin.com/in/chenyishuang/",children:"Yishuang"}),e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#yishuang",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
 `,e.jsx(n.p,{children:"John is a really good team player, among his good qualities, I admire three most."}),`
 `,e.jsxs(n.ol,{children:[`
 `,e.jsx(n.li,{children:"Helpful and warm-hearted whenever you ask for help, John will give you the best help."}),`
